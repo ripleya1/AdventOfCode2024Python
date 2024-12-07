6d11223511eebe2c6baa7cfec494f930ee1d4964
Aaron Ripley <ripleya1@xavier.edu>
2024-12-06 20:03:07 -0500

    Day 3 complete

--- day3.py
+++ day3.py
@@ -2,24 +2,23 @@ import re
 
 sum = 0
 
-def isValidMul(expression):
-    if(re.search("^(d(o((n('(t)?(\((\)?)?)?)?)?)?)?)$", expression) != None):
+def isValidDoOrDont(expression):
+    if(re.search("^d(?:o(?:n(?:'?t?)?)?)?\(?\)?$", expression)):
         return True
     return False
 
 def isCompletelyValidDoOrDont(expression):
-    if(re.search("^do(n't)?\(\)$", expression) != None):
+    if(re.search("do(n't)?\(\)", expression)):
         return True
     return False
 
 def isValidMul(expression):
-    if(re.search("^(m(u(l(\(([0-9]+(,([0-9]+(\)?)?)?)?)?)?)?)?)$", expression) != None):
+    if(re.search("^(m(u(l(\(([0-9]+(,([0-9]+(\)?)?)?)?)?)?)?)?)$", expression)):
         return True
     return False
 
 def isCompletelyValidMul(expression):
-    if(re.search("(mul)\([0-9]+,[0-9]+\)", expression) != None):
-        print(expression)
+    if(re.search("(mul)\([0-9]+,[0-9]+\)", expression)):
         return True
     return False
 
@@ -28,15 +27,34 @@ def getNumsFromValidExpression(expression):
     return [int(temp[0]), int(temp[1].split(")")[0])]
 
 with open("day3input.txt") as f:
+    enabled = True
     for line in f:
         currExpression = ""
+        mul = True
+                
         for char in line:
-            if(len(currExpression) == 0 and (char != 'm' or char != 'd')):
-                continue
+            if(len(currExpression) == 0):
+                if(char != 'm' and char != 'd'):
+                    continue
+                elif(char == 'm'):
+                    mul = True
+                elif(char == 'd'):
+                    mul = False
+
             currExpression += char
-            if(not isValidMul(currExpression)):
+            
+            if(not mul and not isValidDoOrDont(currExpression)):
                 currExpression = ""
-            elif(isCompletelyValidMul(currExpression)):
+            elif(not mul and isCompletelyValidDoOrDont(currExpression)):
+                if(currExpression == "do()"):
+                    enabled = True
+                elif(currExpression == "don't()"):
+                    enabled = False
+                currExpression = ""
+            
+            if((mul and not isValidMul(currExpression)) or (mul and not enabled)):
+                currExpression = ""
+            elif(mul and enabled and isCompletelyValidMul(currExpression)):
                 nums = getNumsFromValidExpression(currExpression)
                 sum += (nums[0] * nums[1])
                 currExpression = ""
